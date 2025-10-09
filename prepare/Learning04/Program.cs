6220c3ea520549ec18c22db002dab9615c99c393
kadenboam <boa24015@byui.edu>
2025-10-08 20:47:47 -0600

    L4 assignment class added and tested

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        //Simple test of Assignment class
+        Assignment a1 = new Assignment("Henry Beckett", "Physics");
+        Console.WriteLine(a1.GetSummary());
     }
 }
