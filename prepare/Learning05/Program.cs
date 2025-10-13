a5b1324ef41934da3861ffb8528cf01b774c07e3
kadenboam <boa24015@byui.edu>
2025-10-13 12:37:59 -0600

    L5 Added square class with simple test

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 5);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
     }
 }
